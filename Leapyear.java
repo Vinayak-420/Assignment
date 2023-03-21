4db65e2f37eab59d0a313b7e726369eb448b9510
Vinayak Avasekar <112852709+Vinayak-420@users.noreply.github.com>
2023-03-21 10:06:47 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,17 @@
+class Leapyear
+{  
+public static void main(String[] args)   
+{    
+int year=2000;  
+ 
+if(year %4 ==0)  
+{  
+System.out.println("The year is Leap year.");  
+}  
+ 
+else   
+{  
+System.out.println("The year is not Leap year.");  
+}    
+}  
+}  
---
+++ Positivenegative.java
@@ -0,0 +1,17 @@
+class Positivenegative 
+{  
+public static void main(String[] args)   
+{    
+int num=9;  
+ 
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+ 
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}    
+}  
+}  
